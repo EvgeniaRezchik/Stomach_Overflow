611a6ba06b4ac5b723bb968013c6a93ecb4c296d
EvgeniaRezchik <144226772+EvgeniaRezchik@users.noreply.github.com>
2025-10-27 00:04:44 +0200

    Delete db.js

--- db.js
+++
@@ -1,5 +0,0 @@
-const mySQL = require("mysql2");
-const fs = require("fs");
-const db = mySQL.createConnection(JSON.parse(fs.readFileSync(__dirname + "/config.json", "utf-8")));
-module.exports = db;
-
