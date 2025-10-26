1b8eda1041d9305ce344abde5be44d3d1fdf9846
EvgeniaRezchik <144226772+EvgeniaRezchik@users.noreply.github.com>
2025-10-27 00:04:20 +0200

    Delete routers directory

--- routers/notifications.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const controller = require("../controllers/notifications");
-const router = express.Router();
-router.get("/", controller.getNotifications);
-router.patch("/:notification_id", controller.readNotification);
-router.delete("/:notification_id", controller.deleteNotification);
-module.exports = router;
-
